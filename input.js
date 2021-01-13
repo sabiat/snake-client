6d2699c12d9d2b3d076607bcacdba2e1a05b98c0
Sabia Tahir <sabiatahir@hotmail.com>
2021-01-13 20:10:48 +0000

    Implemented special keys to send canned messages to server while playing

--- input.js
+++ input.js
@@ -2,7 +2,12 @@
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
+
+//Stores the active TCP connection object.
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -13,11 +18,34 @@ const setupInput = function() {
   return stdin;
 }
 
-// will check for Ctrl + c and exit game
+// will check for Ctrl + c and exit game and handle w,a,s,d keys
 const handleUserInput = function(input) {
   if (input === '\u0003') {
     process.exit();
   }
+  if (input === '\u0077') {
+    connection.write("Move: up");
+  }
+  if (input === '\u0061') {
+    connection.write("Move: left");
+  }
+  if (input === '\u0073') {
+    connection.write("Move: down");
+  }
+  if (input === '\u0064') {
+    connection.write("Move: right");
+  }
+  if (input === '\u006B') {
+    connection.write("Say: hello ☺️");
+  }
+  if (input === '\u006A') {
+    connection.write("Say: how are you");
+  }
+  if (input === '\u006C') {
+    connection.write("Say: I will win!!!");
+  }
 }
+
+
 
 module.exports = {setupInput};
--- play.js
+++ play.js
@@ -3,7 +3,8 @@ const { setupInput } = require('./input');
 
 
 console.log('Connecting ...');
-connect();
+let output = connect();
 
-setupInput();
+//passing the connection object returned from connect to allow for messages to be sent to the server
+setupInput(output);
 
